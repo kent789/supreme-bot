9ea085e6d42d9e92e5bf606c9d52185d9436d751
Kent Kubo <kentkubo1995@gmail.com>
2021-11-18 17:44:32 -0800

    use soup to extract item link

--- src/scripts/supreme_extract.py
+++ src/scripts/supreme_extract.py
@@ -6,9 +6,15 @@ baseUrl = "https://www.supremenewyork.com/shop/all/tops_sweaters"
 
 
 
-# r = requests.get(baseUrl)
-# soup = bs(r.content, 'html.parser')
+r = requests.get(baseUrl)
+soup = bs(r.content, 'html.parser')
+names = soup.select('div.product-name')
+styles = soup.select('div.product-style')
 
+links = [baseUrl + x.find('a')["href"] for x in soup.select('div.inner-article')]
+
+for name, style, link in zip(names, styles, links):
+    print(f"Name: {name.text}, Style: {style.text}, Link: {link}")
 
 def open_browser():
     options = webdriver.ChromeOptions()
@@ -19,7 +25,7 @@ def open_browser():
     # driver.find_element_by_id('Glitter').click()
     driver.find_element_by_link_text('Glitter S/S Top').click()
 
-open_browser()
+# open_browser()
 
 
 
