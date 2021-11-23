ab82490030040cd49a2d74aff38ed514d63696b2
Kent Kubo <kentkubo1995@gmail.com>
2021-11-23 11:09:20 -0800

    add helper function

---
+++ src/scripts/helpers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3 
+import os
+import platform
+import glob
+
+def get_driver_path(driver):
+    supported = ["macOS"]
+    op = platform.platform().split("-")
+    if "x86_64" not in op:
+        os.error("32-bit is not supported at the moment")
+
+    if op[0] in supported:
+        relative_path = "drivers/{}/{}".format(op[0], driver)
+    return relative_path
